a63b0710210c0a47fc3da9ac6b963da0c8d434e3
Dmitriy Schukin <schukindmitriy@gmail.com>
2024-01-17 16:43:56 +0100

    fixes start scripts

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@
 echo "Начало скрипта..."
 
 # Замените следующие строки на реальные ссылки на ваши репозитории
-BACKEND_REPO_URL="git@github.com:DmitriyS87/test-shopify-backend.git"
-FRONTEND_REPO_URL="git@github.com:DmitriyS87/test-shopify-frontend.git"
+BACKEND_REPO_URL="https://github.com/DmitriyS87/test-shopify-backend.git"
+FRONTEND_REPO_URL="https://github.com/DmitriyS87/test-shopify-frontend.git"
+BACKEND_DIR="backend"
+FRONTEND_DIR="frontend"
 
 echo "Клонирование репозитория с бэкенд приложением..."
-git clone $BACKEND_REPO_URL
+git clone $BACKEND_REPO_URL $BACKEND_DIR
 echo "Клонирование завершено."
 
 echo "Клонирование репозитория c фронтенд приложением..."
-git clone $FRONTEND_REPO_URL
+git clone $FRONTEND_REPO_URL $FRONTEND_DIR
 echo "Клонирование завершено."
 
 cd .
@@ -20,10 +22,5 @@ echo "Запуск Docker Compose..."
 docker-compose up --build -d
 echo "Docker Compose успешно запущен."
 
-# Проверка статуса контейнеров может быть добавлена здесь, если требуется
-sleep 60 # - примерное время ожидания запуска контейнеров
-
-echo "Проверка статусов контейнеров..."
 docker-compose ps
-
 echo "Скрипт завершил работу."
